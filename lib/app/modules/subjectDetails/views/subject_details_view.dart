5522493db600f4b2a3f361368f663b2b978d1b14
Pankaj7277 <pankajkr.v21@gmail.com>
2025-03-12 16:09:45 +0530

    updated subject details ui

--- lib/app/modules/subjectDetails/views/subject_details_view.dart
+++ lib/app/modules/subjectDetails/views/subject_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import '../controllers/subject_details_controller.dart';
 import 'package:easy_stepper/easy_stepper.dart';
@@ -7,13 +8,14 @@ class SubjectDetailsView extends GetView<SubjectDetailsController> {
   final String chapter;
   SubjectDetailsView({required this.chapter, super.key});
 
-  int activeStep = 0;
-  int activeStep2 = 0;
-  int reachedStep = 0;
-  int upperBound = 5;
-  double progress = 0.2;
-  double lineLength = 10;
-  Set<int> reachedSteps = <int>{0, 2, 4, 5};
+  final int activeStep = 0;
+  final int activeStep2 = 0;
+  final int reachedStep = 0;
+  final int upperBound = 5;
+  final double progress = 0.2;
+  final double lineLength = 10;
+  final  Set<int> reachedSteps = <int>{0, 2, 4, 5};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,11 +26,24 @@ class SubjectDetailsView extends GetView<SubjectDetailsController> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return ExpansionTile(title: Text("Title Here"));
-              }),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100.h,
+                width: double.infinity,
+                child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dummy Text"),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 10,
+                    shrinkWrap: true,
+                    physics: ScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return ExpansionTile(title: Text("Title Here"));
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
       // body: EasyStepper(
